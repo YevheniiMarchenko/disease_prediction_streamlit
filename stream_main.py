947491d93ab8e85aab6721be38bf82a2bd756915
Yevhenii Marchenko <76454423+YevheniiMarchenko@users.noreply.github.com>
2023-03-24 16:39:27 +0200

    Update stream_main.py

--- stream_main.py
+++ stream_main.py
@@ -55,6 +55,8 @@ Prediction of 3 classes (*Healthy*, *Irritable bowel syndrome*, *T2D Prediabetes
 """)
 
 uploaded_file = st.file_uploader("Choose a file (only .pkl)")
+if uploaded_file is not None:
+    test_4m = pd.read_csv(uploaded_file)
 
 list_4_mers = pd.read_pickle("datafile_4_mers_2100_per_class_3_classes.pkl")
 df_4m = pd.DataFrame(list_4_mers)
@@ -62,8 +64,6 @@ dna_4m = df_to_lists(df_4m)
 fr_4m = occurrence_frequency(dna_4m)
 fr_4m_trg = pd.concat([fr_4m, df_4m[['class']]], axis=1)
 
-with open(uploaded_file, 'rb') as handle:
-    test_4m = pd.read_csv(handle)
 df_test = pd.DataFrame(test_4m)
 df_test.drop('class', axis=1, inplace=True)
 seq_test = df_to_lists(df_test)
